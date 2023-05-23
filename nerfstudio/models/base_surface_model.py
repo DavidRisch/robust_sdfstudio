Fix WandbWriter.write_image() by removing normalization

nerfstudio/models/base_surface_model.py
@@ -646,18 +646,22 @@ class SurfaceModel(Model):
             else:
                 assert False, original.dtype
 
+            # print_tensor(f"log_with_colormap {log_group_name}/{name} image", image)
+            # print_tensor(f"log_with_colormap {log_group_name}/{name} blank_mask", blank_mask)
             image_without_blanks = torch.clone(image)
             image_without_blanks[blank_mask] = 0
+            # print_tensor(f"log_with_colormap {log_group_name}/{name} image_without_blanks", image_without_blanks)
             if torch.max(image_without_blanks) > 0:
                 normalized_image = image_without_blanks / torch.max(image_without_blanks)
             else:
                 normalized_image = image_without_blanks
+            # print_tensor(f"log_with_colormap {log_group_name}/{name} normalized_image", normalized_image)
             colored_loss = colormaps.apply_colormap(normalized_image, cmap=cmap)
             blank_color = torch.tensor([0.6, 0.6, 0.6], dtype=colored_loss.dtype)
 
             blank_mask = blank_mask.reshape(blank_mask.shape[:2])
             colored_loss[blank_mask] = blank_color
-            # print_tensor("log_with_colormap colored_loss", colored_loss)
+            # print_tensor(f"log_with_colormap {log_group_name}/{name} colored_loss", colored_loss)
             writer.put_image(name=log_group_name + "/" + name, image=colored_loss, step=step)
 
         log_with_colormap("pixelwise rgb loss", loss_collection_spatial.pixelwise_rgb_loss)

nerfstudio/utils/writer.py
@@ -28,10 +28,13 @@ import wandb
 from rich.console import Console
 from torch.utils.tensorboard import SummaryWriter
 from torchtyping import TensorType
+from PIL import Image as PILImage
+import numpy as np
 
 from nerfstudio.configs import base_config as cfg
 from nerfstudio.utils.decorators import check_main_thread, decorate_all
 from nerfstudio.utils.printing import human_format
+from nerfstudio.robust.print_utils import print_tensor_dict, print_tensor
 
 CONSOLE = Console(width=120)
 to8b = lambda x: (255 * torch.clamp(x, min=0, max=1)).to(torch.uint8)
@@ -288,8 +291,14 @@ class WandbWriter(Writer):
         wandb.init(project="sdfstudio", name=experiment_name, dir=str(log_dir), reinit=True)
 
     def write_image(self, name: str, image: TensorType["H", "W", "C"], step: int) -> None:
-        image = torch.permute(image, (2, 0, 1))
-        wandb.log({name: wandb.Image(image)}, step=step)
+
+        # Need to create PIL image manually so that wandb does not normalize the image
+        image = image.numpy()
+        image = image * 255
+        image = image.astype(np.uint8)
+        pil_image = PILImage.fromarray(image)
+
+        wandb.log({name: wandb.Image(pil_image)}, step=step)
 
     def write_scalar(self, name: str, scalar: Union[float, torch.Tensor], step: int) -> None:
         wandb.log({name: scalar}, step=step)