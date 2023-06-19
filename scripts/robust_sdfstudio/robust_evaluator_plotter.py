8df5b0659c4b16981a642e6157f5b6c68f509b80
David Risch <acc_mid@rischnet.de>
2023-06-19 20:04:36 +0200

    Add RobustEvaluatorPlotter.plot_aggregated_mask_evaluator_results()

--- scripts/robust_sdfstudio/robust_evaluator_plotter.py
+++ scripts/robust_sdfstudio/robust_evaluator_plotter.py
@@ -8,6 +8,8 @@ import yaml
 from dataclasses import dataclass
 from pathlib import Path
 from typing import Optional, Dict, Any, List
+from collections import defaultdict
+
 import matplotlib.pyplot as plt
 
 import tyro
@@ -52,6 +54,49 @@ class RobustEvaluatorPlotter:
             fig.savefig(plot_directory_path / f"mask_evaluator_{loss_type_name}_{suffix}.png")
 
     @classmethod
+    def plot_aggregated_mask_evaluator_results_part(cls, confusion_name: str, data_by_configuration_name: Dict,
+                                                    # labels: List[str], averages: List[float],
+                                                    ax: plt.axes.Axes):
+        print(f"{data_by_configuration_name=}")
+
+        labels: List[str] = []
+        values: List[float] = []
+
+        for configuration_name, data_for_configuration_dict in data_by_configuration_name.items():
+            print(f"{data_for_configuration_dict=}")
+            raw_values = data_for_configuration_dict[f"{confusion_name}s_list"]
+            value = sum(raw_values) / len(raw_values)
+            labels.append(configuration_name)
+            values.append(value)
+
+        ax.set_title(confusion_name)
+        ax.barh(
+            labels,
+            values,
+        )
+        ax.set_xlabel("average proportion of total")
+
+    @classmethod
+    def plot_aggregated_mask_evaluator_results(cls, data_by_configuration_name_by_loss_type: Dict[str, Dict[str, Any]],
+                                               plot_directory_path: Path):
+
+        for loss_type_name, data_by_configuration_name in data_by_configuration_name_by_loss_type.items():
+            fig, axs = plt.subplots(2, 2, figsize=(12, 12))
+
+            for confusion_name, ax_index in [("true_clean", (0, 0)),
+                                             ("false_clean", (1, 0)),
+                                             ("true_distractor", (1, 1)),
+                                             ("false_distractor", (0, 1))]:
+                # value_list = mask_evaluator_results[f"{confusion_name}s_list"]
+                cls.plot_aggregated_mask_evaluator_results_part(confusion_name=confusion_name,
+                                                                data_by_configuration_name=data_by_configuration_name,
+                                                                ax=axs[ax_index[0]][ax_index[1]])
+
+            plt.tight_layout()
+            # fig.show()
+            fig.savefig(plot_directory_path / f"mask_evaluator_{loss_type_name}_aggregated.png")
+
+    @classmethod
     def plot_for_configuration(cls, data_for_configuration_dict: Dict[str, Any], plot_directory_path: Path) -> None:
         print("data_for_configuration_dict", data_for_configuration_dict)
         plot_directory_path.mkdir(parents=True, exist_ok=True)
@@ -62,9 +107,19 @@ class RobustEvaluatorPlotter:
     @classmethod
     def plot_all(cls, data_dict: Dict[str, Any], plot_directory_path: Path) -> None:
         print("data_dict", data_dict)
-        for name, data_for_configuration_dict in data_dict["configurations"].items():
+
+        data_by_configuration_name_by_loss_type = defaultdict(lambda: defaultdict(dict))
+
+        for configuration_name, data_for_configuration_dict in data_dict["configurations"].items():
             cls.plot_for_configuration(data_for_configuration_dict=data_for_configuration_dict,
                                        plot_directory_path=plot_directory_path)
+
+            for loss_type_name, mask_evaluator_results in data_for_configuration_dict["mask_evaluator_results"].items():
+                data_by_configuration_name_by_loss_type[loss_type_name][configuration_name] = mask_evaluator_results
+
+        cls.plot_aggregated_mask_evaluator_results(
+            data_by_configuration_name_by_loss_type=data_by_configuration_name_by_loss_type,
+            plot_directory_path=plot_directory_path)
 
     def main(self) -> None:
         with open(self.yaml_path, "r") as in_file:
