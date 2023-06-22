e910ec0e732eec903fd31295d0d486f0f621d438
David Risch <acc_mid@rischnet.de>
2023-06-22 23:45:59 +0200

    Fix RobustLossMaskCombiner

--- nerfstudio/robust/robust_loss_mask_combiner.py
+++ nerfstudio/robust/robust_loss_mask_combiner.py
@@ -2,6 +2,7 @@ import torch
 from typing import TYPE_CHECKING
 
 from nerfstudio.robust.loss_collection_dense_spatial import LossCollectionDenseSpatial
+from nerfstudio.robust.print_utils import print_tensor
 
 if TYPE_CHECKING:
     from nerfstudio.models.base_surface_model import SurfaceModelConfig
@@ -13,25 +14,29 @@ class RobustLossMaskCombiner:
     """
 
     @classmethod
-    def combine(cls, loss_collection: LossCollectionDenseSpatial, count_required_for_distracted: int,
+    def combine(cls, loss_collection: LossCollectionDenseSpatial, distracted_votes_required_for_distracted_result: int,
                 device: torch.device):
-        sum_of_masks = torch.zeros_like(loss_collection.rgb_mask, device=device)
+        clean_votes = torch.zeros_like(loss_collection.rgb_mask, device=device)
 
         # print_tensor("before combine rgb", loss_collection.rgb_mask)
 
-        sum_of_masks += loss_collection.rgb_mask
-        sum_of_masks += loss_collection.depth_mask
-        sum_of_masks += loss_collection.normal_mask
+        clean_votes += loss_collection.rgb_mask
+        clean_votes += loss_collection.depth_mask
+        clean_votes += loss_collection.normal_mask
 
-        # print_tensor("sum_of_masks", sum_of_masks)
+        # print_tensor("clean_votes", clean_votes)
 
-        new_mask = (sum_of_masks >= count_required_for_distracted).float()
+        distracted_votes = 3 - clean_votes
+        # print_tensor("distracted_votes", distracted_votes)
 
-        # print_tensor("new_mask", new_mask)
+        new_distracted_mask = (distracted_votes >= distracted_votes_required_for_distracted_result).float()
+        new_clean_mask = torch.logical_not(new_distracted_mask).float()
 
-        loss_collection.rgb_mask = new_mask
-        loss_collection.depth_mask = new_mask
-        loss_collection.normal_mask = new_mask
+        # print_tensor("new_clean_mask", new_clean_mask)
+
+        loss_collection.rgb_mask = new_clean_mask
+        loss_collection.depth_mask = new_clean_mask
+        loss_collection.normal_mask = new_clean_mask
 
     @classmethod
     @torch.no_grad()
@@ -41,10 +46,13 @@ class RobustLossMaskCombiner:
         if config.robust_loss_combine_mode == "Off":
             return
         elif config.robust_loss_combine_mode == "AnyDistracted":  # a pixel if distracted, if *any* of the components are distracted
-            cls.combine(loss_collection=loss_collection, count_required_for_distracted=1, device=device)
+            cls.combine(loss_collection=loss_collection, distracted_votes_required_for_distracted_result=1,
+                        device=device)
         elif config.robust_loss_combine_mode == "AllDistracted":  # a pixel if distracted, if *every* single component is distracted
-            cls.combine(loss_collection=loss_collection, count_required_for_distracted=3, device=device)
+            cls.combine(loss_collection=loss_collection, distracted_votes_required_for_distracted_result=3,
+                        device=device)
         elif config.robust_loss_combine_mode == "Majority":  # a pixel if distracted, if *most* of the components are distracted
-            cls.combine(loss_collection=loss_collection, count_required_for_distracted=2, device=device)
+            cls.combine(loss_collection=loss_collection, distracted_votes_required_for_distracted_result=2,
+                        device=device)
         else:
             raise RuntimeError("Unknown value for robust_loss_combine_mode: " + config.robust_loss_combine_mode)
